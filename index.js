d3bc29e3825c927a82e0938a23418daba3b5b7f3
“Keith <KeithRichardinTech@gmail.com>
2022-01-27 11:02:14 -0500

    imported restaurants.json

---
+++ index.js
@@ -0,0 +1,9 @@
+// import our restaurants
+const restaurants = require("./restaurants.json");
+console.log(restaurants[3].name);
+
+//connect to Firestore
+
+//Create a collcetion called "restaurants"
+
+// add each restaurant
